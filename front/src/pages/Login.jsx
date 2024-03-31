412723df93a362baec8d0421a69db5b5e8117678
alain17-web <web2020.alain@gmail.com>
2024-03-31 18:18:59 +0200

    ADD UX Login

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,10 +1,42 @@
 
  const Login = () => {
     return (
-        <div>
-            <p>Login</p>
+        <div className="w-full h-[95vh] bg-[#A3A3A3] py-5">
+            <form className="w-[60%] h-[auto] flex flex-col mx-auto bg-white my-12 rounded-xl gap-8">
+                <img src="./img/y.jpg" alt="y logo" width={"100px"} className="mt-5 ml-[48%]"/>
+                <h1 className=" text-start text-5xl font-inter font-bold mt-4 ml-[10%]">Sign in to Y</h1>
+                <button
+                    className="flex w-[50%] h-14 mx-auto mt-6 border-2 border-gray-200 justify-center items-center rounded-3xl px-6 cursor-pointer">
+                    <img src="img/google.png" alt="google logo" width="30px" height="30px" className="mr-2"/>
+                    <p className="text-xl font-inter font-bold">Sign up with Google</p>
+                </button>
+                <button
+                    className="flex w-[50%] h-14 mx-auto mt-4 border-2 border-gray-200 justify-center items-center rounded-3xl px-6 cursor-pointer">
+                    <img src="img/apple-logo.png" alt="apple logo" width="30px" height="30px"/>
+                    <p className="text-xl font-inter font-bold">Sign up with Apple</p>
+                </button>
+
+                <p className="text-center text-2xl mt-6">or</p>
+
+                <input
+                    className="w-[60%] h-[58px] mx-auto border-2 border-gray-200  px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black"
+                    type="text" name="username" placeholder="Username" required/>
+                <input
+                    className="w-[60%] h-[58px] mx-auto border-2 border-gray-200  px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black"
+                    type="password" name="pwd" placeholder="Password" required/>
+                <button
+                    className="flex w-[50%] h-16 mx-auto mt-12 text-white text-xl font-inter font-bold justify-center items-center rounded-3xl px-6 cursor-pointer bg-black">
+                    Sign in
+                </button>
+                <button
+                    className="flex w-[50%] h-14 mx-auto mt-4 border-2 border-gray-200 justify-center items-center rounded-3xl px-6 cursor-pointer">
+                    <p className="text-xl font-inter font-bold">Forgot Password</p>
+                </button>
+                <p className="text-center text-xl font-inter mb-4">Don't have an account? <span className="text-[#1B8CD8] cursor-pointer">Sign up</span></p>
+            </form>
+
         </div>
     );
-};
+ };
 
-export default Login
+ export default Login
--- front/src/pages/Register.jsx
+++ front/src/pages/Register.jsx
@@ -4,10 +4,10 @@ const Register = () => {
             <form className="w-[60%] h-[auto] flex flex-col mx-auto bg-white my-24 rounded-xl gap-10">
                 <img src="./img/y.jpg" alt="y logo" width={"100px"} className="mt-5 ml-[48%]"/>
                 <h1 className=" text-start text-5xl font-inter font-bold mt-8 ml-[10%]">Create your account</h1>
-                <input className="w-[60%] h-[58px] mx-auto border-2 border-gray-200 px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black" type="text" name="lastName" placeholder="Last Name"/>
-                <input className="w-[60%] h-[58px] mx-auto border-2 border-gray-200  px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black" type="text" name="firstName" placeholder="First Name"/>
-                <input className="w-[60%] h-[58px] mx-auto border-2 border-gray-200  px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black" type="text" name="username" placeholder="Username"/>
-                <input className="w-[60%] h-[58px] mx-auto border-2 border-gray-200  px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black" type="password" name="pwd" placeholder="Password"/>
+                <input className="w-[60%] h-[58px] mx-auto border-2 border-gray-200 px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black" type="text" name="lastName" placeholder="Last Name" required/>
+                <input className="w-[60%] h-[58px] mx-auto border-2 border-gray-200  px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black" type="text" name="firstName" placeholder="First Name" required/>
+                <input className="w-[60%] h-[58px] mx-auto border-2 border-gray-200  px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black" type="text" name="username" placeholder="Username" required/>
+                <input className="w-[60%] h-[58px] mx-auto border-2 border-gray-200  px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black" type="password" name="pwd" placeholder="Password" required/>
                 <button
                     className="flex w-[50%] h-16 mx-auto my-12  border border-[#CFD9DE] text-white text-xl font-inter font-bold justify-center items-center rounded-3xl px-6 cursor-pointer bg-[#8E8F91]">
                     Sign up

a24902b05c063b3ab84614690c0c21abcbe73a00
alain17-web <web2020.alain@gmail.com>
2024-04-03 01:32:27 +0200

    ADD methods to api

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,9 +1,46 @@
-import {Link} from "react-router-dom"
+import {Link,useNavigate} from "react-router-dom"
 import Footer from "../components/Footer.jsx";
+import {useContext, useState} from "react";
+import axios from "axios";
+import {AuthContext} from "../context/AuthContext.jsx";
 
 const Login = () => {
+
+    const navigate = useNavigate()
+
+    const [credentials, setCredentials] = useState({
+        username: "",
+        pwd:""
+    })
+
+    const {token,loading,error,dispatch} = useContext(AuthContext)
+
+    const handleChange = (e) => {
+        setCredentials(prev=>({...prev,[e.target.id]:e.target.value}))
+    }
+
+    const handleClick = async (e) => {
+        e.preventDefault()
+        dispatch({type:"LOGIN_START"})
+        try{
+            const res = await axios.post("http://localhost:3000/api/users/login",credentials)
+            const token = res.data.token
+
+            localStorage.setItem("token",token)
+            console.log("server response",res.data)
+            dispatch({type:"LOGIN_SUCCESS",payload:token})
+
+            navigate("/dashboard")
+        } catch(err){
+            console.log("login error",err)
+            dispatch({type:"LOGIN_FAILURE",payload:err.response.data})
+
+        }
+    }
+
     return (
         <div className="w-full h-[100vh] bg-[#A3A3A3] py-5">
+            {error && <span className={"text-red-500 text-xl"}>Incorrect credentials</span>}
             <form className="w-[60%] h-[auto] flex flex-col mx-auto bg-white my-12 rounded-xl gap-8">
                 <Link to="/">
                     <img src="./img/y.jpg" alt="y logo" width={"100px"} className="mt-5 ml-[48%] cursor-pointer"/>
@@ -11,12 +48,12 @@ const Login = () => {
                 <h1 className=" text-start text-5xl font-inter font-bold mt-4 ml-[10%]">Sign in to Y</h1>
                 <button
                     className="flex w-[50%] h-14 mx-auto mt-6 border-2 border-gray-200 justify-center items-center rounded-3xl px-6 cursor-pointer">
-                    <img src="img/google.png" alt="google logo" width="30px" height="30px" className="mr-2"/>
+                    <img src="./img/google.png" alt="google logo" width="30px" height="30px" className="mr-2"/>
                     <p className="text-xl font-inter font-bold">Sign up with Google</p>
                 </button>
                 <button
                     className="flex w-[50%] h-14 mx-auto mt-4 border-2 border-gray-200 justify-center items-center rounded-3xl px-6 cursor-pointer">
-                    <img src="img/apple-logo.png" alt="apple logo" width="30px" height="30px"/>
+                    <img src="./img/apple-logo.png" alt="apple logo" width="30px" height="30px"/>
                     <p className="text-xl font-inter font-bold">Sign up with Apple</p>
                 </button>
 
@@ -24,20 +61,35 @@ const Login = () => {
 
                 <input
                     className="w-[60%] h-[58px] mx-auto border-2 border-gray-200  px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black"
-                    type="text" name="username" placeholder="Username" required/>
+                    type="text"
+                    name="username"
+                    id="username"
+                    placeholder="Username"
+                    required
+                    onChange={handleChange}
+                />
                 <input
                     className="w-[60%] h-[58px] mx-auto border-2 border-gray-200  px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black"
-                    type="password" name="pwd" placeholder="Password" required/>
+                    type="password"
+                    name="pwd"
+                    id="pwd"
+                    placeholder="Password"
+                    required
+                    onChange={handleChange}
+                />
                 <button
                     className="flex w-[50%] h-16 mx-auto mt-12 text-white text-xl font-inter font-bold justify-center items-center rounded-3xl px-6 cursor-pointer bg-black">
                     Sign in
                 </button>
                 <button
-                    className="flex w-[50%] h-14 mx-auto mt-4 border-2 border-gray-200 justify-center items-center rounded-3xl px-6 cursor-pointer">
+                    className="flex w-[50%] h-14 mx-auto mt-4 border-2 border-gray-200 justify-center items-center rounded-3xl px-6 cursor-pointer"
+                    onClick={handleClick}
+                >
                     <p className="text-xl font-inter font-bold">Forgot Password</p>
                 </button>
                 <p className="text-center text-xl font-inter mb-4">Don't have an account? <Link to="/register"
-                    className="text-[#1B8CD8] cursor-pointer">Sign up</Link></p>
+                                                                                                className="text-[#1B8CD8] cursor-pointer">Sign
+                    up</Link></p>
             </form>
             <Footer/>
         </div>
--- front/src/pages/Register.jsx
+++ front/src/pages/Register.jsx
@@ -1,18 +1,95 @@
 import {Link} from "react-router-dom"
 import Footer from "../components/Footer.jsx";
+import { useNavigate } from "react-router-dom";
+import {useState} from "react";
+import axios from "axios";
 
 const Register = () => {
+
+    const navigate = useNavigate()
+
+    const [lastName,setLastName] = useState("")
+    const [firstName,setFirstName] = useState("")
+    const [username,setUsername] = useState("")
+    const [pwd,setPwd] = useState("")
+    const [success,setSuccess] = useState("")
+    const [errorMsg,setErrorMsg] = useState("")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try{
+            const userData = {
+                lastName: lastName,
+                firstName: firstName,
+                username: username,
+                pwd: pwd
+            }
+            const res = await axios.post("http://localhost:3000/api/users/register",userData)
+            console.log(res.data)
+            setSuccess("Sign up successful. You can now sign in")
+            setErrorMsg("")
+            navigate("/login")
+        }catch (err){
+            console.log(err)
+            setSuccess("")
+            setErrorMsg("Failed to sign in. Try again")
+        }
+    }
+
     return (
         <div className="w-full h-[100vh] bg-[#A3A3A3] py-5">
-            <form className="w-[60%] h-[auto] flex flex-col mx-auto bg-white mt-24 mb-44 rounded-xl gap-10">
+            {success && (
+                <span className={"text-green-400 text-xl"}>{success}</span>
+            )}
+            {errorMsg && (
+                <span className={"text-red-400 text-xl"}>{errorMsg}</span>
+            )}
+
+            <form className="w-[60%] h-[auto] flex flex-col mx-auto bg-white mt-24 mb-44 rounded-xl gap-10"
+                  onSubmit={handleSubmit}
+            >
                 <Link to="/">
                     <img src="./img/y.jpg" alt="y logo" width={"100px"} className="mt-5 ml-[48%] cursor-pointer"/>
                 </Link>
                 <h1 className=" text-start text-5xl font-inter font-bold mt-8 ml-[10%]">Create your account</h1>
-                <input className="w-[60%] h-[58px] mx-auto border-2 border-gray-200 px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black" type="text" name="lastName" placeholder="Last Name" required/>
-                <input className="w-[60%] h-[58px] mx-auto border-2 border-gray-200  px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black" type="text" name="firstName" placeholder="First Name" required/>
-                <input className="w-[60%] h-[58px] mx-auto border-2 border-gray-200  px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black" type="text" name="username" placeholder="Username" required/>
-                <input className="w-[60%] h-[58px] mx-auto border-2 border-gray-200  px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black" type="password" name="pwd" placeholder="Password" required/>
+                <input className="w-[60%] h-[58px] mx-auto border-2 border-gray-200 px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black"
+                       type="text"
+                       name="lastName"
+                       id="lastName"
+                       placeholder="Last Name"
+                       required
+                       value={lastName}
+                       onChange={(e)=>setLastName(e.target.value)}
+                />
+                <input className="w-[60%] h-[58px] mx-auto border-2 border-gray-200 px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black"
+                       type="text"
+                       name="firstName"
+                       id="firstName"
+                       placeholder="First Name"
+                       required
+                       value={firstName}
+                       onChange={(e)=>setFirstName(e.target.value)}
+                />
+                <input className="w-[60%] h-[58px] mx-auto border-2 border-gray-200 px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black"
+                       type="text"
+                       name="userame"
+                       id="username"
+                       placeholder="Username"
+                       required
+                       value={username}
+                       onChange={(e)=>setUsername(e.target.value)}
+                />
+
+
+                <input className="w-[60%] h-[58px] mx-auto border-2 border-gray-200  px-4 placeholder:text-xl placeholder:font-thin placeholder:text-black"
+                       type="password"
+                       name="pwd"
+                       id="pwd"
+                       placeholder="Password"
+                       required
+                       value={pwd}
+                       onChange={(e)=>setPwd(e.target.value)}
+                />
                 <button
                     className="flex w-[50%] h-16 mx-auto mt-12 mb-24 text-white text-xl font-inter font-bold justify-center items-center rounded-3xl px-6 cursor-pointer bg-black">
                     Sign up
